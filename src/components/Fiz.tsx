13cb1e4fb71e6706429399ec3029c1d71d4e11c4
Vasilii <v.ionichev@gmail.com>
2023-02-12 17:54:51 +0300

    styles updated

--- src/components/Fiz.tsx
+++ src/components/Fiz.tsx
@@ -4,43 +4,64 @@ import {
   EuiFormRow,
   EuiFieldText,
   EuiButton,
-  EuiTextArea,
+  EuiText,
+  EuiSplitPanel,
 } from "@elastic/eui";
 
 const Fiz = () => {
   return (
     <div>
-      <EuiFlexGroup style={{ marginTop: '60px', marginBottom: '60px' }}>
+      <EuiFlexGroup
+        style={{ marginTop: "60px", marginBottom: "60px", color: "#36A2EF" }}
+      >
         <EuiFlexItem>
-          <EuiFormRow label="Фамилия" style={{fontSize:'20px'}}>
-            <EuiFieldText />
+          <EuiFormRow label="Фамилия" style={{ fontSize: "20px" }}>
+            <EuiFieldText style={{height: '30px'}}/>
           </EuiFormRow>
         </EuiFlexItem>
         <EuiFlexItem>
-          <EuiFormRow label="Имя" style={{fontSize:'20px'}}>
-            <EuiFieldText />
+          <EuiFormRow label="Имя" style={{ fontSize: "20px" }}>
+            <EuiFieldText style={{height: '30px'}}/>
           </EuiFormRow>
         </EuiFlexItem>
         <EuiFlexItem>
-          <EuiFormRow label="Отчество" style={{fontSize:'20px'}}>
-            <EuiFieldText />
+          <EuiFormRow label="Отчество" style={{ fontSize: "20px" }}>
+            <EuiFieldText style={{height: '30px'}}/>
           </EuiFormRow>
         </EuiFlexItem>
         <EuiFlexItem>
-          <EuiFormRow label="Дата рождения" style={{fontSize:'20px'}}>
-            <EuiFieldText />
-          </EuiFormRow>
-        </EuiFlexItem>
-        <EuiFlexItem>
-          <EuiFormRow hasEmptyLabelSpace>
-            <EuiButton style={{fontSize:'20px'}}>Запросить данные</EuiButton>
+          <EuiFormRow label="Дата рождения" style={{ fontSize: "20px" }}>
+            <EuiFieldText style={{height: '30px'}}/>
           </EuiFormRow>
         </EuiFlexItem>
       </EuiFlexGroup>
       <EuiFlexGroup>
-        <EuiFormRow label="Результат" style={{fontSize:'20px'}}>
-          <EuiTextArea />
-        </EuiFormRow>
+        <EuiFlexItem grow={false}>
+          <EuiButton fill style={{ fontSize: "20px" }}>
+            Запросить данные
+          </EuiButton>
+        </EuiFlexItem>
+        <EuiFlexItem>
+          <EuiSplitPanel.Outer>
+            <EuiSplitPanel.Inner style={{ color: "#36A2EF" }}>
+              <EuiFlexGroup>
+                <EuiFlexItem>
+                  <EuiText>Результат запроса</EuiText>
+                </EuiFlexItem>
+                <EuiFlexItem grow={false}>
+                  <EuiButton iconType="copyClipboard" iconSide="left">
+                    Скопировать
+                  </EuiButton>
+                </EuiFlexItem>
+              </EuiFlexGroup>
+            </EuiSplitPanel.Inner>
+            <EuiSplitPanel.Inner color="subdued">
+              <EuiText>
+                <p className="ResultFiz"></p>
+              </EuiText>
+            </EuiSplitPanel.Inner>
+          </EuiSplitPanel.Outer>
+        </EuiFlexItem>
       </EuiFlexGroup>
     </div>
   );
--- src/components/Yur.tsx
+++ src/components/Yur.tsx
@@ -2,29 +2,52 @@ import {
   EuiFormRow,
   EuiFieldText,
   EuiFlexGroup,
-  EuiTextArea,
   EuiButton,
   EuiFlexItem,
+  EuiSplitPanel,
+  EuiText,
 } from "@elastic/eui";
-
 
 const Yur = () => {
   return (
     <div>
-      <EuiFlexGroup style={{ marginTop: "60px", marginBottom: "60px"}}>
-        <EuiFormRow label="Наименование организации" style={{fontSize:'20px'}}>
-          <EuiFieldText name="company-name" />
+      <EuiFlexGroup
+        style={{ marginTop: "60px", marginBottom: "60px", color: "#36A2EF" }}
+      >
+        <EuiFormRow
+          label="Наименование организации"
+          style={{ fontSize: "20px" }}
+        >
+          <EuiFieldText name="company-name" style={{height: '30px'}}/>
         </EuiFormRow>
-        <EuiFlexItem>
-          <EuiFormRow hasEmptyLabelSpace>
-            <EuiButton style={{fontSize:'20px'}}>Запросить данные</EuiButton>
-          </EuiFormRow>
-        </EuiFlexItem>
       </EuiFlexGroup>
       <EuiFlexGroup>
-        <EuiFormRow label="Результат" style={{fontSize:'20px'}}>
-          <EuiTextArea />
-        </EuiFormRow>
+        <EuiFlexItem grow={false}>
+          <EuiButton fill style={{ fontSize: "20px" }}>
+            Запросить данные
+          </EuiButton>
+        </EuiFlexItem>
+        <EuiFlexItem>
+          <EuiSplitPanel.Outer>
+            <EuiSplitPanel.Inner style={{ color: "#36A2EF" }}>
+              <EuiFlexGroup>
+                <EuiFlexItem>
+                  <EuiText>Результат запроса</EuiText>
+                </EuiFlexItem>
+                <EuiFlexItem grow={false}>
+                  <EuiButton iconType="copyClipboard" iconSide="left">
+                    Скопировать
+                  </EuiButton>
+                </EuiFlexItem>
+              </EuiFlexGroup>
+            </EuiSplitPanel.Inner>
+            <EuiSplitPanel.Inner color="subdued">
+              <EuiText>
+                <p className="ResultYur"></p>
+              </EuiText>
+            </EuiSplitPanel.Inner>
+          </EuiSplitPanel.Outer>
+        </EuiFlexItem>
       </EuiFlexGroup>
     </div>
   );
